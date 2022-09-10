19035c9153b7c0c5d289141dc25dab98b28a4cae
jephersonnn <ericjeffrey.sch@gmail.com>
2022-09-11 07:52:31 +0800

    Complete blogsite exercise

--- app.js
+++ app.js
@@ -3,12 +3,23 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+const mongoose = require("mongoose");
 const _ = require('lodash'); //LODASH node module import
 //lodash opens up javascript functions, especially ones that can be used on strings, to be used on node
+
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
 const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
+
+mongoose.connect("mongodb://localhost:27017/BlogPosts");
+
+const postSchema = {
+  title: String,
+  content: String
+};
+
+const Post = mongoose.model("Post",postSchema);
 
 const app = express();
 
@@ -22,10 +33,18 @@ app.use(bodyParser.urlencoded({
 app.use(express.static("public"));
 
 app.get("/", function(req, res) {
-  res.render("home.ejs", {
-    sectionText: homeStartingContent,
-    renderPosts: allPosts});
+
+  Post.find({ }, function(err, foundPosts){
+    res.render("home.ejs",{
+      sectionText: homeStartingContent,
+      renderPosts: foundPosts
+    })
   });
+
+  // res.render("home.ejs", {
+  //   sectionText: homeStartingContent,
+  //   renderPosts: allPosts});
+});
 
 app.get("/about", function(req, res) {
   res.render("about.ejs", {
@@ -44,24 +63,39 @@ app.get("/compose", function(req, res) {
 })
 
 app.post("/compose", function(req, res) {
-  const newPostContent = {
+  const newPost =  new Post ({
     title: req.body.postTitle,
-    body: req.body.postBody,
-  };
+    content: req.body.postBody,
+  });
 
-  allPosts.push(newPostContent);
-
-  res.redirect("/");
+  newPost.save(function(err){
+    if (!err){
+      res.redirect("/");
+    }
+    else {
+      console.log("Something happened!");
+    }
+  });
+  console.log("Post succesfully saved!");
 })
 
-app.get("/posts/:postTitle", function(req, res) { //Express Routing Parameters
-  allPosts.forEach (function (post){
-    if (_.lowerCase(post.title) === _.lowerCase(req.params.postTitle))
-    res.render("post.ejs",{
-      postTitle: post.title,
-      postBody: post.body
-    })
-  });
+app.get("/posts/:postId", function(req, res) { //Express Routing Parameters
+  console.log(req.params.postId);
+
+ Post.findOne({_id: req.params.postId}, function (err, post){
+   res.render("post.ejs", {
+     postTitle: post.title,
+     postBody: post.content
+   })
+ })
+
+  // allPosts.forEach (function (post){
+  //   if (_.lowerCase(post.title) === _.lowerCase(req.params.postTitle))
+  //   res.render("post.ejs",{
+  //     postTitle: post.title,
+  //     postBody: post.body
+  //   })
+  // });
 })
 
 
